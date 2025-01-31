fe3d50eeab9bde3e6a2ffbb08719e94a883f43fc
subinkim <a01072669755@gmail.com>
2025-01-31 15:21:06 +0900

    Add multiply

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ function sub(a,b){
     return a-b;
 }
 
+function mul(a,b){
+
+    return a*b;
+}
+
 function testadd(a,b){
 
 console.log(add(1,1)==2);
@@ -19,3 +24,4 @@ function testsub(a,b){
     console.log(sub(2,1)==1);
         
 }
+
